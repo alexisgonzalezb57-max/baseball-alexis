d26e43795cb6cb7bafe3a64cedc5190f07fcd620
MistCode <arturovielma41@gmail.com>
2025-11-14 13:51:36 -0400

    mas cambios
    otros cambios

--- modulos/stats/jgcalcular.php
+++ modulos/stats/jgcalcular.php
@@ -68,6 +68,15 @@ if ($avg_multi === 0) {
 }
 
 
+
+
+
+$constastar = "SELECT * FROM resumen_stats WHERE id_player = $id_player";
+$confkj = mysqli_query($con, $constastar);
+$vjt = mysqli_num_rows($confkj);
+if ($vjt >= 1) {
+
+
 $configurar = "UPDATE resumen_stats SET tnj = '$tnj',
                                         vb  = '$tvb',
                                         h   = '$th',
@@ -93,5 +102,119 @@ $configurar = "UPDATE resumen_stats SET tnj = '$tnj',
 $resav = mysqli_query($con, $configurar);
 
 Header("Location: ../entradas/datos.php?id_tab=$id_tab&nj=$id_nj");
+
+} elseif ($vjt < 1) {
+    
+$vb        = 0;
+$h         = 0;
+$hr        = 0;
+$dosb      = 0;
+$tresb     = 0;
+$ca        = 0;
+$ci        = 0;
+$k         = 0;
+$b         = 0;
+$a         = 0;
+$jjg       = 0;
+$tvb       = 0;
+$cp        = 0;
+$cpl       = 0;
+$il        = 0;
+$th        = 0;
+$avg       = 0;
+$efec      = 0;
+$cb        = 0;
+$va        = 0;
+$sf        = 0;
+$br        = 0;
+$gp        = 0;
+$ile       = 0;
+
+
+$stats = "SELECT * FROM jugadores WHERE id_player = $id_player;";
+$stquy = mysqli_query($con, $stats);
+$numns = mysqli_num_rows($stquy);
+      if ($numns >= 1) {
+        for ($jg=1; $jg <= $numns ; $jg++) { 
+          $player       = mysqli_fetch_array($stquy);
+          $nombre       = $player['nombre'];
+          $apellido     = $player['apellido'];
+          $cedula       = $player['cedula'];
+          $id_player    = $player['id_player'];
+
+          $jugador      = $nombre." ".$apellido;
+  
+          $guardarstats = "INSERT INTO resumen_stats  SET id_tab   = '$id_tab',
+                                                      id_temp      = '$id_temp',
+                                                      id_team      = '$id_team',
+                                                      id_player    = '$id_player',
+                                                      cedula       = '$cedula',
+                                                      name_jgstats = '$jugador',
+                                                      tnj          = '$nj',
+                                                      categoria    = '$categoria',
+                                                      vb           = '$vb',
+                                                      h            = '$h',
+                                                      hr           = '$hr',
+                                                      2b           = '$dosb',
+                                                      3b           = '$tresb',
+                                                      ca           = '$ca',
+                                                      ci           = '$ci',
+                                                      k            = '$k',
+                                                      b            = '$b',
+                                                      a            = '$a',
+                                                      sf           = '$sf',
+                                                      br           = '$br',
+                                                      gp           = '$gp',
+                                                      tvb          = '$tvb',
+                                                      th           = '$th',
+                                                      avg          = '$avg',
+                                                      cb           = '$cb';";
+          $resaves = mysqli_query($con, $guardarstats);
+        }
+      }
+
+
+$configurar = "UPDATE resumen_stats SET tnj = '$tnj',
+                                        vb  = '$tvb',
+                                        h   = '$th',
+                                        hr  = '$thr',
+                                        2b  = '$t2b',
+                                        3b  = '$t3b',
+                                        ca  = '$tca',
+                                        ci  = '$tci',
+                                        k   = '$tk',
+                                        b   = '$tb',
+                                        a   = '$ta',
+                                        sf  = '$tfl',
+                                        br  = '$tbr',
+                                        gp  = '$tgp',
+                                        tvb = '$dtvb',
+                                        th  = '$dth',
+                                        avg = '$avg',
+                                        cb  = $cb 
+                                        WHERE id_tab    = $id_tab 
+                                        AND   id_team   = $id_team 
+                                        AND   id_player = $id_player 
+                                        AND   cedula    LIKE '%$cedula%'; ";
+$resav = mysqli_query($con, $configurar);
+
+Header("Location: ../entradas/datos.php?id_tab=$id_tab&nj=$id_nj");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
